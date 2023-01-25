5422c02d5dca9310b88001d4870b16977d2b4119
Evgeniy <s.h.a.r.k.112@mail.ru>
2023-01-26 02:28:17 +0700

    add method of 1st array and changed array

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿string[] ChangedArray(string[] array)
+{
+    int c = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            c++;
+        }
+    }
+    string[] SecondArray = new string[c];
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            SecondArray[j] = array[i];
+            j++;
+        }
+    }
+    return SecondArray;
+}
