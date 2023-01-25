48be68a09d11176567353dca0c3a53a1fac96e95
Evgeniy <s.h.a.r.k.112@mail.ru>
2023-01-26 02:28:40 +0700

    add method of print

--- Program.cs
+++ Program.cs
@@ -20,3 +20,13 @@
     }
     return SecondArray;
 }
+
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write($"{array[array.Length - 1]}]");
+}
